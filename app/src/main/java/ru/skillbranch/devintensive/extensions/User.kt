df251f063ebf0f6aded0e53cef101b925c378ce6
Ivan Skiridomov <ivan.skiridomov@gmail.com>
2019-07-08 14:50:58 +0300

    fix transliteration

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -3,11 +3,10 @@ package ru.skillbranch.devintensive.extensions
 import ru.skillbranch.devintensive.models.User
 import ru.skillbranch.devintensive.models.UserView
 import ru.skillbranch.devintensive.utils.Utils
-import java.util.*
 
 fun User.toUserView(): UserView {
 
-    val nickname = Utils.translitiration("$firstName $lastName")
+    val nickname = Utils.transliteration("$firstName $lastName")
     val initials = Utils.toInitials(firstName, lastName)
     val status = if(lastVisit == null) "Еще не разу не был" else if (isOnline) "online" else "Последний раз был ${lastVisit?.humanizeDiff()}"
 
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -17,7 +17,7 @@ object Utils {
         return firstName to lastName
     }
 
-    fun translitiration(payload: String, divider: String = " "): String {
+    fun transliteration(payload: String, divider: String = " "): String {
         val lettersMap: HashMap<Char, String> = hashMapOf(
             'а' to "a", 'б' to "b", 'в' to "v", 'г' to "g", 'д' to "d", 'е' to "e", 'ё' to "e", 'ж' to "zh", 'з' to "z",
             'и' to "i", 'й' to "i", 'к' to "k", 'л' to "l", 'м' to "m", 'н' to "n", 'о' to "o", 'п' to "p", 'р' to "r",
